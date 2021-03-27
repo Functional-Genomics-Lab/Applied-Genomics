f05de4d9a76434a4b90ec8c0897a16b6a3bb5cc6
Edmund Miller <edmund.a.miller@protonmail.com>
2021-03-26 21:33:42 -0500

    feat: Add script to download samples

---
+++ download_samples.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002248/SRR002248.fastq.gz -o SRR002248_Illumina_sequencing_of_Mouse_NP_H3K4me2_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002247/SRR002247.fastq.gz -o SRR002247_Illumina_sequencing_of_Mouse_NP_H3K4me2_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR001/SRR001643/SRR001643.fastq.gz -o SRR001643_Illumina_sequencing_of_Mouse_MCV6_H3K27me3_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR001/SRR001644/SRR001644.fastq.gz -o SRR001644_Illumina_sequencing_of_Mouse_MEF_H3K4me3_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002249/SRR002249.fastq.gz -o SRR002249_Illumina_sequencing_of_Mouse_NP_H3K4me2_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002250/SRR002250.fastq.gz -o SRR002250_Illumina_sequencing_of_Mouse_NP_H3K4me1_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR001/SRR001645/SRR001645.fastq.gz -o SRR001645_Illumina_sequencing_of_Mouse_MEF_H3K4me3_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR029/SRR029192/SRR029192.fastq.gz -o SRR029192_Illumina_sequencing_of_Mouse_MCV6_H3K4me3_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR001/SRR001642/SRR001642.fastq.gz -o SRR001642_Illumina_sequencing_of_Mouse_MCV6_H3K4me3_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002251/SRR002251.fastq.gz -o SRR002251_Illumina_sequencing_of_Mouse_NP_H3K4me1_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002253/SRR002253.fastq.gz -o SRR002253_Illumina_sequencing_of_Mouse_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002256/SRR002256.fastq.gz -o SRR002256_Illumina_sequencing_of_Mouse_Brain_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002257/SRR002257.fastq.gz -o SRR002257_Illumina_sequencing_of_Mouse_Brain_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002252/SRR002252.fastq.gz -o SRR002252_Illumina_sequencing_of_Mouse_NP_H3K4me1_ChIPSeq_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002255/SRR002255.fastq.gz -o SRR002255_Illumina_sequencing_of_Mouse_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002254/SRR002254.fastq.gz -o SRR002254_Illumina_sequencing_of_Mouse_genomic_fragment_library.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR002/SRR002258/SRR002258.fastq.gz -o SRR002258_Illumina_sequencing_of_Mouse_Brain_genomic_fragment_library.fastq.gz
