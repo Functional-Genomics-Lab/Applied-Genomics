308c8c581cf9ff55f42d570f1b874e46bd22e56c
Edmund Miller <edmund.a.miller@protonmail.com>
2021-05-24 16:03:51 -0500

    fix: Remove intro

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ module.exports = {
       items: [
         {
           type: 'doc',
-          docId: 'intro',
+          docId: 'overview',
           position: 'left',
           label: 'Course',
         },
@@ -39,7 +39,7 @@ module.exports = {
           items: [
             {
               label: 'Overview',
-              to: '/docs/intro',
+              to: '/docs/overview',
             },
           ],
         },
