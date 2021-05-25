e741521ebd3fcdb11ca78ed0f58b263c3f47aa4f
Edmund Miller <edmund.a.miller@protonmail.com>
2021-05-25 08:49:56 -0500

    fix: Update landing page

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -6,36 +6,23 @@ const FeatureList = [
   {
     title: 'Data Analysis',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    description: <>Data exploration, hypothesis generation, and biases. </>,
   },
   {
     title: 'Reproducible Research',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
+      <>How to utilize https://nf-co.re/ to avoid reinventing the wheel. </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Genomics',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    description: <>We'll explore the durability of genomic data. </>,
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
